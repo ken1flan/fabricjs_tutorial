f9f661f654d08d8c48122d33bf502fdbe2c93e7d
ken1flan <ken1flan@gmail.com>
2015-04-20 16:59:46 +0900

    練習用のhtmlひな形

---
+++ practices/sample.js
@@ -0,0 +1,35 @@
+$(initialize);
+
+function initialize () {
+  display_select_photo();
+  $(".link_select_photo").click(display_select_photo);
+  $(".link_select_groom_face").click(display_select_groom_face);
+  $(".link_select_bride_face").click(display_select_bride_face);
+}
+
+function display_select_photo () {
+  $(".link_select_photo").parent().addClass("active");
+  $(".link_select_groom_face").parent().removeClass("active");
+  $(".link_select_bride_face").parent().removeClass("active");
+  $(".select_photo").show();
+  $(".select_groom_face").hide();
+  $(".select_bride_face").hide();
+}
+
+function display_select_groom_face () {
+  $(".link_select_photo").parent().removeClass("active");
+  $(".link_select_groom_face").parent().addClass("active");
+  $(".link_select_bride_face").parent().removeClass("active");
+  $(".select_photo").hide();
+  $(".select_groom_face").show();
+  $(".select_bride_face").hide();
+}
+
+function display_select_bride_face () {
+  $(".link_select_photo").parent().removeClass("active");
+  $(".link_select_groom_face").parent().removeClass("active");
+  $(".link_select_bride_face").parent().addClass("active");
+  $(".select_photo").hide();
+  $(".select_groom_face").hide();
+  $(".select_bride_face").show();
+}
